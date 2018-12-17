621af3f9d863a3444d9859adc23a3325ff2c7e53
Alex Williams and James Cho <alex+james@>
2018-12-17 12:46:38 -0800

    Added nav bars and pages for each search.

--- JobSearch/Models/Job.cs
+++ JobSearch/Models/Job.cs
@@ -11,27 +11,6 @@ namespace JobSearch.Models
 {
     public class JobSearchClass
     {
-        // Initialize the Chrome Driver
-        public static void RunSearch()
-        {
-
-            ChromeDriver driver = new ChromeDriver("/Users/Guest/Desktop/JobSearch.Solution/JobSearch/wwwroot/drivers");
-            
-            // Go to the home page
-            driver.Navigate().GoToUrl("http://www.google.com");
-
-            // Get the page elements
-            var searchForm = driver.FindElementByName("q");
-
-            // Type user name and password
-            searchForm.SendKeys("How do I do selenium stuff?");
-
-            // and click the login button
-            searchForm.Submit();
-
-            // // Extract the text and save it into result.txt
-            // var result = driver.FindElementByXPath("//div[@id='case_login']/h3").Text;
-            // File.WriteAllText("result.txt", result);
-        }
+       
     }
 }
