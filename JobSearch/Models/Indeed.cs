29564f51143eff15cad3e5bbbae3b94457b3cc2b
Leilani Leach and Manasa Vesala <leilani@>
2018-12-17 15:43:30 -0800

    Added Indeed.cs code

--- JobSearch/Models/Indeed.cs
+++ JobSearch/Models/Indeed.cs
@@ -7,6 +7,7 @@ using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 namespace JobSearch.Models
 {
     public class IndeedClass
@@ -34,48 +35,75 @@ namespace JobSearch.Models
         {
 
             ChromeDriver driver = new ChromeDriver("/Users/Guest/Desktop/JobSearch.Solution/JobSearch/wwwroot/drivers");
-            
+
             // Go to the home page
             driver.Navigate().GoToUrl("http://www.indeed.com");
 
             // Get the page elements
             var searchForm = driver.FindElementByName("q");
             var locationForm = driver.FindElementByName("l");
-            
+
 
             // Type user name and password
             searchForm.SendKeys(jobName);
             locationForm.SendKeys("");
-            for(int i =0; i <20; i++)
+            for (int i = 0; i < 20; i++)
             {
-               locationForm.SendKeys(Keys.Backspace);
+                locationForm.SendKeys(Keys.Backspace);
             }
             locationForm.SendKeys(jobLocation);
 
             // and click the login button
             searchForm.Submit();
-            
-            List<IndeedClass> indeedJobs = new List<IndeedClass>{};
-            string tempTitle ="";
+
+            List<IndeedClass> indeedJobs = new List<IndeedClass> { };
+            string tempTitle = "";
             string tempLink = "";
-            
-            for(int i =1; i < 8; i++)
+
+            for (int i = 1; i < 8; i++)
             {
-                var tempListing = driver.FindElementById("sja" +i); 
+                var tempListing = driver.FindElementById("sja" + i);
                 tempTitle = tempListing.Text;
                 tempLink = tempListing.GetAttribute("href");
                 IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
                 indeedJobs.Add(tempJob);
             }
 
-          
+            var nextLink = driver.FindElementByXPath("//*[@id='resultsCol']/div[28]/a[1]/span");
+            nextLink.Click();
+
+            while (indeedJobs.Count < 10)
+            {
+                try
+                {
+                    for (int j = 7; j < 8; j++)
+                    {
+                        var tempListing2 = driver.FindElementById("sja" + j);
+                        tempTitle = tempListing2.Text;
+                        tempLink = tempListing2.GetAttribute("href");
+                        IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+                        indeedJobs.Add(tempJob);
+                    }
+                }
+                catch
+                {
+                    for (int j = 7; j < 8; j++)
+                    {
+                        var thirdLink = driver.FindElementByXPath("//*[@id='resultsCol']/div[28]/a[3]");
+                        var tempListing3 = driver.FindElementById("sja" + j);
+                        tempTitle = tempListing3.Text;
+                        tempLink = tempListing3.GetAttribute("href");
+                        IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+                        indeedJobs.Add(tempJob);
+                    }
+
+
+                }
+            }
+
 
 
             return indeedJobs;
-   
-            // // Extract the text and save it into result.txt
-            // var result = driver.FindElementByXPath("//div[@id='case_login']/h3").Text;
-            // File.WriteAllText("result.txt", result);
         }
     }
 }
