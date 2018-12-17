669c08bba504db5790b58b4059e25b20ba196a0f
[hwisoo] <[hwisoocho@gmail.com]>
2018-12-17 15:18:00 -0800

    Can make a list of 10 entries

--- JobSearch/Models/Indeed.cs
+++ JobSearch/Models/Indeed.cs
@@ -7,6 +7,7 @@ using System.Text;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 
+
 namespace JobSearch.Models
 {
     public class IndeedClass
@@ -59,7 +60,7 @@ namespace JobSearch.Models
             string tempTitle ="";
             string tempLink = "";
             
-            for(int i =1; i < 5; i++)
+            for(int i =1; i < 8; i++)
             {
                 var tempListing = driver.FindElementById("sja" +i); 
                 tempTitle = tempListing.Text;
@@ -68,7 +69,62 @@ namespace JobSearch.Models
                 indeedJobs.Add(tempJob);
            }
 
-          
+            var nextLink = driver.FindElementByXPath("//*[@id='resultsCol']/div[28]/a[1]/span");
+                        nextLink.Click();
+
+           while(indeedJobs.Count <10)
+           {
+               try
+               {
+                    for(int j= 7; j<8; j++)   
+                   {
+                        var tempListing2 = driver.FindElementById("sja" +j); 
+                        tempTitle = tempListing2.Text;
+                        tempLink = tempListing2.GetAttribute("href");
+                        IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+                        indeedJobs.Add(tempJob);
+                   }  
+               }
+               catch
+               {
+                   for(int j= 7; j<8; j++)  
+                   {
+                        var thirdLink = driver.FindElementByXPath("//*[@id='resultsCol']/div[28]/a[3]");
+                        var tempListing3 = driver.FindElementById("sja" +j); 
+                        tempTitle = tempListing3.Text;
+                        tempLink = tempListing3.GetAttribute("href");
+                        IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+                        indeedJobs.Add(tempJob);
+                   }
+                
+
+               }
+           }
+
+   
+       
+        // IList<IWebElement> listOflinks = new List<IWebElement>();
+
+        // for(int i = 0 ; i < links.Count ; i++)
+        // {
+        //     tempTitle = links[i].Text;
+        //     tempLink = links[i].GetAttribute("href");
+        //     IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+        //     indeedJobs.Add(tempJob);
+        // }       
+
+        
+  
+
+        // WebBrowser wb = new WebBrowser();
+        // List<HtmlElement> links = wb.Document.getElementsByTagAndClassName("div", "error");
+        // foreach(HtmlElement link in links)
+        // {
+        //     tempTitle = tempListing.Text;
+        //     tempLink = tempListing.GetAttribute("href");
+        //     IndeedClass tempJob = new IndeedClass(tempTitle, tempLink);
+        //     indeedJobs.Add(tempJob);
+        // }       
 
 
             return indeedJobs;
