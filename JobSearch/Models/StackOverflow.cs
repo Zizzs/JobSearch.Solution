c3c01d1c61748d2e5b9950eebdce0f45b9902405
Leilani Leach and Manasa Vesala <manasa@>
2018-12-18 14:10:04 -0800

    cleaned the code

--- JobSearch/Models/StackOverflow.cs
+++ JobSearch/Models/StackOverflow.cs
@@ -58,13 +58,6 @@ namespace JobSearch.Models
             List<StackOverflow> stackOverflowJobs = new List<StackOverflow> { };
             string tempTitle = "";
             string tempLink = "";
-            int timeout = 0;
-            // while (driver.FindElements(By.XPath("//*[@id='mainbar']/div[2]/div/div[3]")).Count == 0 && timeout < 500)
-            // {
-            //     Console.WriteLine("timing out");
-            //     Thread.Sleep(2000);
-            //     timeout++;
-            // }
             bool existsElement(int i)
             {
                 try
@@ -85,12 +78,10 @@ namespace JobSearch.Models
                     {
                         Console.WriteLine("first try started");
                         var tempListing = driver.FindElementByXPath("//*[@id='mainbar']/div[2]/div/div[" + i + "]/div[3]/div[1]/h2/a");
-                        // var tempListing = driver.FindElementByPartialLinkText("developer");
                         tempTitle = tempListing.Text;
                         tempLink = tempListing.GetAttribute("href");
                         StackOverflow tempJob = new StackOverflow(tempTitle, tempLink);
                         stackOverflowJobs.Add(tempJob);
-                        Console.WriteLine("finished first try");
                     }
                 }
             }
@@ -98,49 +89,16 @@ namespace JobSearch.Models
             {
                 for (int i = 1; i < 20; i++)
                 {
-                    Console.WriteLine("catch triggered");
                     if (existsElement(i))
                     {
                         var tempListing = driver.FindElementByXPath("//*[@id='mainbar']/div[2]/div/div[" + i + "]/div[3]/div[1]/h2/a");
-                        // var tempListing = driver.FindElementByPartialLinkText("developer");
                         tempTitle = tempListing.Text;
                         tempLink = tempListing.GetAttribute("href");
                         StackOverflow tempJob = new StackOverflow(tempTitle, tempLink);
                         stackOverflowJobs.Add(tempJob);
-                        Console.WriteLine("finished second try");
                     }
                 }
             }
-            //     IList<IWebElement> links = driver.FindElements(By.ClassName("s-link"));
-            //     IList<IWebElement> listOflinks = new List<IWebElement>();
-
-            //     for(int i = 0 ; i < links.Count ; i++)
-            //     {
-            //     links = driver.FindElements(By.ClassName("s-link"));
-
-            //     if (!string.IsNullOrEmpty(links[i].Text) && links[i].GetAttribute("data-tn-element") == "jobTitle")
-            //     {
-            //         tempTitle = links[i].Text;
-            //         tempLink = links[i].GetAttribute("href");
-            //         StackOverflow tempJob = new StackOverflow(tempTitle, tempLink);
-            //         stackOverflowJobs.Add(tempJob);
-            //     }
-
-
-            //   }
-
-            // return stackOverflowJobs;
-            // Console.WriteLine(stackOverflowJobs[0]);
-            // // Extract the text and save it into result.txt
-            // // File.WriteAllText("result.txt", resultText);
-            // //*[@id="mainbar"]/div[2]/div/div[8]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[7]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[6]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[3]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[1]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[1]/div[3]/div[1]/h2/a
-            // //*[@id="mainbar"]/div[2]/div/div[3]/div[3]/div[1]/h2/a
-
             return stackOverflowJobs;
         }
     }
