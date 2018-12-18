bfe6f7eeac56a9bdba6240dec516c95c606825d6
aw <Zizzs17@gmail.com>
2018-12-18 10:17:07 -0800

    Added Craigslist and Glassdoor

--- JobSearch/Controllers/JobController.cs
+++ JobSearch/Controllers/JobController.cs
@@ -32,6 +32,7 @@ namespace JobSearch.Controllers
             List<StackOverflow> model = StackOverflow.RunSearch(jobName, jobLocation);
             return View("StackOverflow", model);
         }
+
         [HttpGet("/jobs/indeed")]
         public ActionResult Indeed()
         {
@@ -44,6 +45,35 @@ namespace JobSearch.Controllers
         {
             List<IndeedClass> model = IndeedClass.RunSearch(jobName, jobLocation);
             return View("Indeed", model);
+        }
+
+        [HttpGet("/jobs/glassdoor")]
+        public ActionResult Glassdoor()
+        {
+            List<IndeedClass> model = new List<IndeedClass>();
+            return View(model);
+        }
+
+        [HttpPost("/jobs/glassdoor")]
+        public ActionResult GlassdoorSearch(string jobName, string jobLocation)
+        {
+            List<GlassdoorClass> model = GlassdoorClass.RunSearch(jobName, jobLocation);
+            return View("Glassdoor", model);
+            
+        }
+
+        [HttpGet("/jobs/craigslist")]
+        public ActionResult Craigslist()
+        {
+            List<CraigslistClass> model = new List<CraigslistClass>();
+            return View(model);
+        }
+
+        [HttpPost("/jobs/craigslist")]
+        public ActionResult CraigslistSearch(string jobName)
+        {
+            List<CraigslistClass> model = CraigslistClass.RunSearch(jobName);
+            return View("Craigslist", model);
         }
 
     }
